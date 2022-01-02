348af323fec1ebe0a90d3867dec09dc56782d4b3
Mukund2900 <mukundmundhra0@gmail.com>
2022-01-02 18:34:39 +0530

    V3-

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'add_link_from_mongo_atlas',
+    {useNewUrlParser: true, useUnifiedTopology: true}
+);
+
+const paymentSchema = new mongoose.Schema({
+    id: String,
+    itemId: String,
+    paid: Boolean
+});
+
+const Payment = mongoose.model('Payment', paymentSchema);
+
+module.exports = {
+    Payment
+}
